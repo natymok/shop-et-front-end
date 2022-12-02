a6ae102a84e164fdbc0a19d894c809ab6910ca41
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-02 08:24:29 -0500

    change the server url

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const _token=localStorage.getItem('user')
  const axiosinstance=axios.create({
-    baseURL:"https://etshop-server.onrender.com/api",
+    baseURL:"https://shop-bakend.onrender.com/api",
     headers:{
         "Authorization":_token?_token:''
     }
