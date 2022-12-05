91daa8e495cda0bf434fa69df8482a413c2094ce
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-05 01:56:13 -0500

    chamge the server url

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const _token=localStorage.getItem('user')
  const axiosinstance=axios.create({
-    baseURL:"https://server-mg37.onrender.com/api",
+    baseURL:"http://localhost:3000/api",
     headers:{
         "Authorization":_token?_token:''
     }
