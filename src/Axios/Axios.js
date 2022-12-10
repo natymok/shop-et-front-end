35b4e9e6305f2b984b041f5bf6a265a8b023da98
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-10 07:00:21 -0500

    Access-Control-Allow-Origin

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -3,7 +3,8 @@ const _token=localStorage.getItem('user')
  const axiosinstance=axios.create({
     baseURL:"https://etshop-server.onrender.com/api",
     headers:{
-        "Authorization":_token?_token:''
+        "Authorization":_token?_token:'',
+        "Access-Control-Allow-Origin":'*'
     }
 })
 export default axiosinstance;
