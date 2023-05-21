c3308a4a8da79f997c82202f8990f6abfcd240f3
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-05-21 03:42:18 -0400

    change axios server to deployed url

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const _token=localStorage.getItem('user')
  const axiosinstance=axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://company-server-svea.onrender.com/api",
     headers:{
         "authorization":_token?_token:'',
         "Access-Control-Allow-Origin":'*'
