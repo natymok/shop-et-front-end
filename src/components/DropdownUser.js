eff983c4b3946304b2b6070803667f73a8a9ae58
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-06-03 08:45:55 -0400

    add get report functionaity

--- src/components/DropdownUser.js
+++ src/components/DropdownUser.js
@@ -3,10 +3,40 @@ import { Link } from 'react-router-dom';
 import { useStateValue } from '../Context/StateProvider';
 import UserOne from '../images/user/user-01.png';
 import { useNavigate } from 'react-router-dom';
+import jsPDF from 'jspdf';
 
 const DropdownUser = () => {
+  const [{token,TotalSell,amount,customer,companyName,transaction},dispatch]=useStateValue()
+  const downloadReport=()=>{
+    const reportdata=`companyName==${companyName}
+Total sell=${TotalSell},
+customer=${customer}
+total profit from january-december
+january==${transaction[0]}
+february==${transaction[1]}
+march==${transaction[2]}
+april==${transaction[3]}
+may==${transaction[4]}
+june==${transaction[5]}
+july==${transaction[6]}
+august==${transaction[7]}
+september==${transaction[8]}
+october==${transaction[9]}
+November==${transaction[10]}
+december==${transaction[11]}`
+const doc=new jsPDF()
+doc.text(reportdata,10,10)
+doc.save("report.pdf")
+   /* const filenamne="report.pdf"
+    const fileurl=URL.createObjectURL(new Blob([doc],{type:'application/pdf'}))
+    const link=document.createElement('a')
+    link.href=fileurl
+    link.setAttribute('download',filenamne)
+    document.body.appendChild(link)
+    link.click()*/
+  }
   const navigate = useNavigate();
-  const [{token,TotalSell,amount,customer,companyName},dispatch]=useStateValue()
+ 
   const logout=()=>{
     localStorage.clear()
     dispatch({
@@ -115,6 +145,14 @@ const DropdownUser = () => {
             />
           </svg>
           Log Out
+        </button>
+        <button onClick={downloadReport} className="flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+         <svg  className="bg-white"
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14 2.26953V6.40007C14 6.96012 14 7.24015 14.109 7.45406C14.2049 7.64222 14.3578 7.7952 14.546 7.89108C14.7599 8.00007 15.0399 8.00007 15.6 8.00007H19.7305M9 15L12 18M12 18L15 15M12 18L12 12M14 2H8.8C7.11984 2 6.27976 2 5.63803 2.32698C5.07354 2.6146 4.6146 3.07354 4.32698 3.63803C4 4.27976 4 5.11984 4 6.8V17.2C4 18.8802 4 19.7202 4.32698 20.362C4.6146 20.9265 5.07354 21.3854 5.63803 21.673C6.27976 22 7.11984 22 8.8 22H15.2C16.8802 22 17.7202 22 18.362 21.673C18.9265 21.3854 19.3854 20.9265 19.673 20.362C20 19.7202 20 18.8802 20 17.2V8L14 2Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+          get Report
         </button>
       </div>
       {/* <!-- Dropdown End --> */}
